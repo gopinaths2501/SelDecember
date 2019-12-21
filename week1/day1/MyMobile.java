680b0c0829b870cb89f33ef1450916d172fcd138
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-12-21 19:25:54 +0530

    week 1 day 1 code

---
+++ week1/day1/MyMobile.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class MyMobile {
+
+	public static void main(String[] args) {
+		
+		Mobile obj = new Mobile();
+		obj.takeSnap();
+		obj.addNumber(9876543210L);
+		long num = obj.searchNum("Sam");
+		System.out.println(num);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
