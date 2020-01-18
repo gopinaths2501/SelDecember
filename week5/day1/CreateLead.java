74f3d3e5134d9262b74f6712b5007a84227cff43
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-01-18 13:38:11 +0530

    Setting up individual test data for each testcases

---
+++ week5/day1/CreateLead.java
@@ -0,0 +1,32 @@
+package week4.day2;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import base.ProjectSpecificMethods;
+
+public class CreateLead extends ProjectSpecificMethods {
+	
+	@BeforeTest
+	public void setFileName() {
+		excelFileName = "TestData";
+	}
+	
+	@Test(dataProvider="sendData")
+	public void runCreateLead(String cName, String fName, String lName) {
+
+		driver.findElementByLinkText("Leads").click();
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys(cName);
+		driver.findElementById("createLeadForm_firstName").sendKeys(fName);
+		driver.findElementById("createLeadForm_lastName").sendKeys(lName);
+		driver.findElementByName("submitButton").click();
+
+	}
+}
+
+
+
+
+
+
